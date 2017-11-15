b2de1c7f0c78410289324f281214c03f5ddb7599
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-11-15 18:20:30 +0200

    Add files via upload

---
+++ HomeWorkPart1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class PrintingTheCurrentDateAndTime
+{
+    static void Main()
+    {
+        Console.WriteLine(@"          PROGRAM FOR PRINTING THE ""CURRENT DATE AND TIME"" ");
+        Console.WriteLine("\n============================================================");
+
+        DateTime current = DateTime.Now;
+        Console.WriteLine("\nTHE CURRENT DATE AND TIME IS:{0}",current);
+    }
+}
