77e904f21dfb4fa76089614a6b8f6dc58a19dfe6
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-05 19:47:09 +0200

    Add files via upload

---
+++ NumbersFromNTo0InReverse/NumbersFromNTo0InReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersFromNTo0InReverse
+{
+    public class NumbersFromNto0InReverse
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"      PROGRAM FOR DISPLAYING NUMBERS FROM ""N"" BY USER INPUT TO ""0"" IN REVERSE ORDER ");
+            Console.WriteLine("\n=======================================================================================");
+
+            Console.Write("\nPLEASE, INPUT A NUMBER FROM WHICH YOU WANT TO START THE LOOP: " );
+            int input = int.Parse(Console.ReadLine());
+
+            for( int a = input; a > 0; a--)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
