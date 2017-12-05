8ce4d0fd8743f6ed6e9f71fd9a8b7146a131273d
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-05 22:02:00 +0200

    Add files via upload

---
+++ DisplayingSquareOfSpecialCharacters/DisplayingSquareOfSpecialCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayingSquareOfSpecialCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"          PROGRAM FOR DISPLAYING SQUARE OF SPECIAL CHARACTERS");
+            Console.WriteLine("\n============================================================");
+
+            for(int a = 0; a < 6; a++ )
+            {
+                for(int b = 0; b < 10; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
