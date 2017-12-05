df3dcb5248d86a61ba41f07fff7d436846eb98e5
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-05 23:17:43 +0200

    Add files via upload

---
+++ TriangleOfDollarsWithNestedLoops/TriangleOfDollarsWithNestedLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleOfDollarsWithNestedLoops
+{
+    public class TriangleOfDollarsWithNestedLoops
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"         PROGRAM FOR DISPLAYING TRIANGLE OF ""DOLLARS"" WITH NESTED LOOPS");
+            Console.WriteLine("\n========================================================================");
+
+            for(int a = 0; a <= 10; a++)
+            {
+                for(int b = 0; b < a; b++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
