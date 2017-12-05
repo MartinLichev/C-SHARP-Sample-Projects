fdb7b642e0431afad7db44859f8d9dc7e4b68db4
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-05 12:50:31 +0200

    Add files via upload

---
+++ FindingTheLargestValueWithMethod/FindingTheLargestValueWithMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingTheLargestValueWithMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"  PROGRAM FOR FINDING THE ""LARGEST VALUE"" WITH METHOD");
+            Console.WriteLine("\n======================================================");
+
+            Console.Write("\nPLEASE, INPUT \"1ST\" NUMBER: ");
+            int input1 = int.Parse(Console.ReadLine());
+            Console.Write("\nPLEASE, INPUT \"2ND\" NUMBER: ");
+            int input2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("THE LARGEST OUT OF THE \"TWO\" NUMBERS IS:{0}" ,GetMax(input1,input2));
+            
+
+        }
+
+
+        public static int GetMax(int input1, int input2)
+        {
+            if(input1 > input2)
+            {
+                return input1;
+            }
+            else if(input2 > input1)
+            {
+                return input2;
+            }
+
+            return 0;
+
+            
+        }
+    }
+}
