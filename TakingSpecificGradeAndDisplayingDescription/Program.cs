bddcaf6110ca09779a3e38382c40c629c80b8392
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-06 19:00:47 +0200

    Add files via upload

---
+++ TakingSpecificGradeAndDisplayingDescription/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakingSpecificGradeAndDisplayingDescription
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"   PROGRAM FOR TAKING A SPECIFIC ""GRADE MARK"" AND DISPLAYING THE EQUIVALENT DESCRIPTION");
+            Console.WriteLine("\n========================================================================================");
+
+
+            Console.Write("\nPLEASE, INPUT A SPECIFIC \"GRADE MARK\": ");
+            char input = char.Parse(Console.ReadLine().ToUpper());
+
+            switch(input)
+            {
+                case 'A': Console.WriteLine("\nTHE MARK IS \"EXCELLENT\"!");
+                    break;
+                case 'B': Console.WriteLine("\nTHE MARK IS \"VERY GOOD\"!");
+                    break;
+                case 'C': Console.WriteLine("\nTHE MARK IS \"GOOD\"!");
+                    break;
+                case 'D': Console.WriteLine("\nTHE MARK IS \"DECENT\"!");
+                    break;
+                case 'E': Console.WriteLine("\nTHE MARK IS \"AVERAGE\"!");
+                    break;
+                case 'F': Console.WriteLine("\nTHE MARK IS \"FAIL\"!");
+                    break;
+            }
+                
+        }
+    }
+}
---
+++ TwoNumbersEquality/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoNumbersEquality
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"         PROGRAM FOR CHECKING EQUALITY OF ""TWO"" NUMBERS WITH CONDITIONAL STATEMENT");
+            Console.WriteLine("\n===================================================================================");
+
+
+            Console.Write("\nPLEASE, INPUT \"1ST\" NUMBER: ");
+            int input1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nPLEASE, INPUT \"2ND\" NUMBER: ");
+            int input2 = int.Parse(Console.ReadLine());
+
+
+            if(input1 > input2)
+            {
+                Console.WriteLine("\n\"1ST\" NUMBER - {0} IS GREATER THAN \"2ND\" NUMBER - {1}",input1, input2);
+            }
+            else if(input2 > input1)
+            {
+                Console.WriteLine("\n\"2ND\" NUMBER - {0} IS GREATER THAN \"1ST\" NUMBER - {1}",input2, input1);
+            }
+            else if(input1 == input2)
+            {
+                Console.WriteLine("\nTHE TWO NUMBERS ARE EQUAL!");
+            }
+        }
+    }
+}
