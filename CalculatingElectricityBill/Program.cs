45fda99cb3d2f27a4afc2d3f78c5be37a224ab48
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-06 18:45:21 +0200

    Add files via upload

---
+++ CalculatingElectricityBill/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingElectricityBill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"          PROGRAM FOR CALCULATING ELECTICITY BILL, BASED UPON CONSUMPTION");
+            Console.WriteLine("\n========================================================================");
+
+
+            long id = 0;
+            string name = null;
+            int consumption = 0;
+
+            Console.Write("\nPLEASE, INPUT YOUR \"ID\":");
+            long input1 = long.Parse(Console.ReadLine());
+            id += input1;
+
+            Console.Write("\nPLEASE, INPUT YOUR \"NAME\":");
+            string input2 = Console.ReadLine();
+            name += input2;
+
+            Console.Write("\nPLEASE, INPUT YOUR \"UNIT-CONSUMPTION\":");
+            int input3 = int.Parse(Console.ReadLine());
+            consumption += input3;
+
+            if(input3 <=199)
+            {
+                Console.WriteLine("\nYOUR ELECTRICITY-BILL IS {0}$.",(input3 * 1.20));
+            }
+            else if((input3 >=200) && (input3 <400))
+            {
+                Console.WriteLine("\nYOUR ELECTRICITY-BILL IS {0}$.", (input3 * 1.50));
+            }
+            else if((input3 >=400) && (input3 < 600 ))
+            {
+                Console.WriteLine("\nYOUR ELECTRICITY-BILL IS {0}$.", (input3 * 1.80));
+            }
+            else if(input3 >=600 )
+            {
+                Console.WriteLine("\nYOUR ELECTRICITY-BILL IS {0}$.", (input3 * 2.00));
+            }
+        }
+    }
+}
---
+++ CheckingIfYearIsLeapOrNot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingIfYearIsLeapOrNot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"     PROGRAM FOR CHECKING, IF CERTAIN YEAR IS ""LEAP"" OR ""NOT LEAP"" ");
+            Console.WriteLine("\n======================================================================");
+
+
+            Console.Write("\nPLEASE, INPUT CERTAIN YEAR: ");
+            long input = long.Parse(Console.ReadLine());
+
+            string leapornotleap = (input % 4 == 0) ? "IT IS A LEAP YEAR!" : "IT IS NOT A LEAP YEAR";
+
+            Console.WriteLine(leapornotleap);
+        }
+    }
+}
+
+                    
+
+
+
+
+
+    
