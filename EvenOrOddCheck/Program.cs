e56a764e7fef7bd4f19e6e6acbd7dd9e0bf5f606
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-06 18:57:43 +0200

    Add files via upload

---
+++ EvenOrOddCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOddCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"     PROGRAM FOR CHECKING IF A NUMBER IS ""EVEN"" OR ""ODD"" ");
+            Console.WriteLine("\n============================================================");
+
+            Console.Write("\nPLEASE, INPUT A NUMBER: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if(input % 2 == 0)
+            {
+                Console.WriteLine("\nTHE NUMBER IS \"EVEN\" ");
+            }
+            else if(input % 2 == 1)
+            {
+                Console.WriteLine("\nTHE NUMBER IS \"ODD\" ");
+            }
+        }
+    }
+}
---
+++ FindingTheLargestOutOfThree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingTheLargestOutOfThree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(@"      PROGRAM FOR FINDING THE LARGEST OUT OF ""THREE"" NUMBERS");
+            Console.WriteLine("\n=============================================================");
+
+            Console.Write("\nPLEASE, INPUT \"1ST\" NUMBER: ");
+            int input1 = int.Parse(Console.ReadLine());
+            Console.Write("\nPLEASE, INPUT \"2ND\" NUMBER: ");
+            int input2 = int.Parse(Console.ReadLine());
+            Console.Write("\nPLEASE, INPUT \"3RD\" NUMBER: ");
+            int input3 = int.Parse(Console.ReadLine());
+
+
+            if((input1 > input2) && (input2 > input3))
+            {
+                Console.WriteLine("\n\"1ST\" NUMBER - {0} IS THE LARGEST!", input1);
+            }
+            else if((input2 > input1) && (input2 > input3))
+            {
+                Console.WriteLine("\n\"2ND\" NUMBER - {0} IS THE LARGEST!", input2);
+            }
+            else if((input3 > input1) && (input3 > input2))
+            {
+                Console.WriteLine("\n\"3RD\" NUMBER - {0} IS THE LARGEST!", input3);
+            }
+        }
+    }
+}
