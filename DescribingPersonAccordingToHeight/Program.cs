801d4c7d04d7972551a2661dc22a585bc6d756a7
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-06 18:52:06 +0200

    Add files via upload

---
+++ DescribingPersonAccordingToHeight/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescribingPersonAccordingToHeight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(@"        PROGRAM FOR ACCEPTING PERSON'S HEIGHT AND DESCRIBING HIM ");
+            Console.WriteLine("\n================================================================");
+
+            Console.Write("\nPLEASE, INPUT YOU HEIGHT: ");
+            double input = double.Parse(Console.ReadLine());
+
+            if(input >= 175)
+            {
+                Console.WriteLine("\nYOU ARE TALL!");
+            }
+            else if((input < 175) && (input >= 165))
+            {
+                Console.WriteLine("\nYOU ARE NOT SO TALL!");
+            }
+            else
+            {
+                Console.WriteLine("\nYOU ARE SHORT!");
+            }
+        }
+    }
+}
+
+
+
---
+++ DisplayingCertainNumberInWord/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayingCertainNumberInWord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"         PROGRAM FOR DISPLAYING CERTAIN NUMBER(FROM 1-10) IN WORD!");
+            Console.WriteLine("\n======================================================");
+
+            Console.Write("\nPLEASE, INPUT A NUMBER: ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("\nONE!");
+                    break;
+                case 2:
+                    Console.WriteLine("\nTWO!");
+                    break;
+                case 3:
+                    Console.WriteLine("\nTHREE!");
+                    break;
+                case 4:
+                    Console.WriteLine("\nFOUR!");
+                    break;
+                case 5:
+                    Console.WriteLine("\nFIVE!");
+                    break;
+                case 6:
+                    Console.WriteLine("\nSIX!");
+                    break;
+                case 7:
+                    Console.WriteLine("\nSEVEN!");
+                    break;
+                case 8:
+                    Console.WriteLine("\nEIGHT!");
+                    break;
+                case 9:
+                    Console.WriteLine("\nNINE!");
+                    break;
+                case 10:
+                    Console.WriteLine("\nTEN!");
+                    break;
+                default:
+                    Console.WriteLine("\nINCORRECT FORMAT, PLEASE TRY AGAIN!");
+                    break;
+            }
+        }
+    }
+}
+
