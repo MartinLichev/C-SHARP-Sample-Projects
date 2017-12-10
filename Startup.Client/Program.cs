b1742f447bfbe6d1b8af91d36e4f1b8ea8b47af3
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-11 00:00:33 +0200

    Add files via upload

---
+++ Startup.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem;
+
+namespace Startup.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var data = new LibraryDbContext())
+            {
+
+                Book book = new Book { Author = "Jack", Name = "Billy", Id = 1, ISBNNumber = 10000000, NumberOfPages = 256, YearOfPublishing = 1991 };
+
+                data.Books.Add(book);
+
+                data.SaveChanges();
+            }
+        }
+    }
+}
+            
+                
