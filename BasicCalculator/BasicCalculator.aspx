719f5847ae3a23999e5248056b8e22020b142ca5
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-13 22:06:26 +0200

    Add files via upload

---
+++ BasicCalculator/BasicCalculator.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BasicCalculator.aspx.cs" Inherits="BasicCalculator.BasicCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID ="Calculator" runat="server" Text ="Basic Calculator" Font-Italic ="true" Font-Bold ="true" Font-Size="X-Large"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="FirstNumber" runat="server" Text="FirstNumber:" Font-Bold="true" Font-Italic="true" Font-Size="X-Large"></asp:Label>
+        <asp:TextBox ID="FirstNumberText" runat="server" Height="20px" Width="100px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="SecondNumber" runat="server" Text="SecondNumber:" Font-Bold="true" Font-Italic="true" Font-Size="X-Large"></asp:Label>
+        <asp:TextBox ID="SecondNumberText" runat="server" Height="20px" Width="100px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button runat="server" ID="SumButton" Text="SUM" Height="50px" Width="100px" OnClick="SumButton_Click" />
+        <asp:Button runat ="server" ID="DevideButton" Text="DEVIDE" Height="50px" Width="100px" OnClick="DevideButton_Click" />
+        <asp:Button runat="server" ID="MultipleButton" Text="MULTIPLE" Height="50px" Width="100px" OnClick="MultipleButton_Click" />
+        <asp:Button runat="server" ID="SubtractionButton" Text="SUBSTRACT" Height="50px" Width="100px" OnClick="SubtractionButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="ResultLabel" runat="server" Text="Result:" Font-Size="X-Large" Font-Bold="true" Font-Italic="true"></asp:Label>
+        <asp:TextBox ID="Resulttext" runat="server" Height="20px" Width="100px" ReadOnly ="true"></asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ BasicCalculator/BasicCalculator.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BasicCalculator
+{
+    public partial class BasicCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (this.FirstNumberText.Text == "")
+            {
+                this.FirstNumberText.Text = "0";
+            }
+            if (this.SecondNumberText.Text == "")
+            {
+                this.SecondNumberText.Text = "0";
+            }
+        }
+
+        protected void SumButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double firstnumber = Convert.ToDouble(this.FirstNumberText.Text);
+
+                double secondnumber = Convert.ToDouble(this.SecondNumberText.Text);
+
+                double result = (firstnumber + secondnumber);
+
+                this.Resulttext.Text = result.ToString();
+            }
+            catch (SystemException)
+            {
+                this.Resulttext.Text = "error!";
+            }
+
+
+        }
+
+        protected void DevideButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double firstnumber = Convert.ToDouble(this.FirstNumberText.Text);
+
+                double secondnumber = Convert.ToDouble(this.SecondNumberText.Text);
+
+                double result = (firstnumber / secondnumber);
+
+                this.Resulttext.Text = result.ToString();
+            }
+            catch (SystemException)
+            {
+                this.Resulttext.Text = "error!";
+            }
+
+        }
+
+        protected void MultipleButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double firstnumber = Convert.ToDouble(this.FirstNumberText.Text);
+
+                double secondnumber = Convert.ToDouble(this.SecondNumberText.Text);
+
+                double result = (firstnumber * secondnumber);
+
+                this.Resulttext.Text = result.ToString();
+            }
+            catch (SystemException)
+            {
+                this.Resulttext.Text = "error!";
+            }
+
+        }
+
+        protected void SubtractionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double firstnumber = Convert.ToDouble(this.FirstNumberText.Text);
+
+                double secondnumber = Convert.ToDouble(this.SecondNumberText.Text);
+
+                double result = (firstnumber - secondnumber);
+
+                this.Resulttext.Text = result.ToString();
+            }
+            catch (SystemException)
+            {
+                this.Resulttext.Text = "error!";
+            }
+
+        }
+    }
+}
