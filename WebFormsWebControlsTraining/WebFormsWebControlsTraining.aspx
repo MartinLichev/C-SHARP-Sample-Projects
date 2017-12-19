588ef3f05a267a1afa981bb68d68c92a42ec277c
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-20 01:48:50 +0200

    Add files via upload

---
+++ WebFormsWebControlsTraining/WebFormsWebControlsTraining.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormsWebControlsTraining.aspx.cs" Inherits="WebFormsWebControlsTraining.WebFormsWebControlsTraining" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="margin-left:auto; margin-right:auto; text-align:center" runat="server">
+        <asp:Label runat="server" ID="HeadingLabel" Text="Web Forms - Web Controls Training" Font-Bold="true" Font-Size="XX-Large"></asp:Label>
+        <br />
+        <br />
+        <asp:Label runat="server" ID="ListBoxExample" Text="ListBox Example:" Font-Bold="true" Font-Size="X-Large"></asp:Label>
+        <br />
+        <br />
+        <asp:ListBox runat="server" ID="ListBox" Font-Bold="true" Font-Size="X-Large" Width="400px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Label runat="server" ID="CheckBoxExample" Text="CheckBox Example:" style="margin-left:auto; margin-right:auto; text-align:center" Font-Bold="true" Font-Size="X-Large"></asp:Label>
+        <br />
+        <br />
+        <asp:CheckBoxList runat="server" ID="CheckBoxList"  style="margin-left:auto; margin-right:auto; text-align:center" Font-Size= "X-Large" Font-Bold="true" Width="400px"></asp:CheckBoxList>
+        <br />
+        <br />
+        <asp:Label runat="server" ID="ListBox2Text" style ="margin-left:auto; margin-right:auto; text-align:center" Text="ListBox Example With Objects:" Font-Size="X-Large" Font-Bold="true"></asp:Label>
+        <br />
+        <br />
+        <asp:ListBox runat="server" ID="ListBox2" Font-Bold="true" Font-Size="X-Large" Width="500px" style="margin-left:auto; margin-right:auto; text-align:center" ItemType="Child"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Label runat="server" ID="DropDownText" Font-Bold="true" Font-Size="X-Large" style ="margin-left:auto; margin-right:auto; text-align:center" Text="DropDownList"></asp:Label>
+        <br />
+        <br />
+        <asp:DropDownList runat="server" ID="DropDownList" Font-Bold="true" Font-Size="X-Large" Width="400px" style="margin-left:auto; margin-right:auto; text-align:center"></asp:DropDownList>
+
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebFormsWebControlsTraining/WebFormsWebControlsTraining.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsWebControlsTraining
+{
+    public partial class WebFormsWebControlsTraining : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> names = new List<string>
+            {
+                {"Martin" },
+                {"Dimitar" },
+                {"Elenko" },
+                {"Stanimir" },
+                {"Arsen" },
+                {"Dinko" },
+                {"Hristomir" },
+            };
+
+            if (!Page.IsPostBack)
+            {
+
+                this.ListBox.DataSource = names;
+
+            }
+
+            List<string> townlist = new List<string>
+            { "Sofia", 
+              "Sliven",
+              "Plovdiv",
+              "Varna",
+              "Burgas",
+              "Lom",
+              "Ruse",
+              "Kurdjali",        
+            };
+
+            if(!Page.IsPostBack)
+            {
+                this.CheckBoxList.DataSource = townlist;
+
+            }
+
+            List<Child> childlist = new List<Child>
+            {
+                new Child {Firstname="Petar", Lastname="Petrov", Age=10 },
+                new Child {Firstname ="Georgi", Lastname ="Grigorov", Age =5 },
+                new Child {Firstname ="Atanas", Lastname ="Paunov", Age =7 },
+                new Child {Firstname = "Boris", Lastname ="Sultov", Age = 9 },
+
+            };
+
+            
+            foreach(Child element in childlist)
+            {
+                this.ListBox2.Items.Add($"{element.Firstname} {element.Lastname} {element.Age}");
+            }
+
+
+
+            
+            
+
+
+            List<string> peoplenames = new List<string>
+            {
+                "Martin",
+                "Ivan",
+                "Dragan",
+                "Stefan",
+                "Monika",
+            };
+
+            this.DropDownList.DataSource = peoplenames;
+
+            this.Page.DataBind();
+
+
+
+        }
+    }
+
+    public class Child
+    {
+        private string firstname;
+
+        private string lastname;
+
+        private int age;
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
