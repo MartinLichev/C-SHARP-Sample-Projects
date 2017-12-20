82febcda3d9514ca22e39c90ab3ca921f3762012
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-20 13:14:19 +0200

    Add files via upload

---
+++ CountriesAndCapitals.Models/Capitals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesAndCapitals.Models
+{
+    public class Capitals
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CountryName { get; set; }
+
+        public long Size { get; set; }
+
+        public long Population { get; set; }
+
+        public virtual ICollection<Countries> Countries { get; set; }
+    }
+}
---
+++ CountriesAndCapitals.Models/Countries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesAndCapitals.Models
+{
+    public class Countries
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ContinentLocation { get; set; }
+
+        public long Size { get; set; }
+
+        public long Population { get; set; }
+
+
+    }
+}
