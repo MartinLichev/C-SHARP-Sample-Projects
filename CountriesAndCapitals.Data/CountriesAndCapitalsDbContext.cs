90c68610c2ca2550206af9ad3973f6ca2b941b24
MartinLichev <33695591+MartinLichev@users.noreply.github.com>
2017-12-20 13:13:23 +0200

    Add files via upload

---
+++ CountriesAndCapitals.Data/CountriesAndCapitalsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CountriesAndCapitals.Models;
+
+namespace CountriesAndCapitals.Data
+{
+    public class CountriesAndCapitalsDbContext:DbContext
+    {
+        public CountriesAndCapitalsDbContext(): base("CapitalsAndCountriesDataBase")
+        {
+
+        }
+        public virtual IDbSet<Capitals> Capitals { get; set; }
+
+        public virtual IDbSet<Countries> Countries { get; set; }
+
+
+    }
+}
